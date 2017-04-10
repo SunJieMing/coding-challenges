d699c28557b68adb3a2f5d05fad9aafd12d1625b
Ben Nelson <s.benjamin.nelson@gmail.com>
2017-04-10 09:38:53 -0600

    Update evenOccurences.js

--- evenOccurences/evenOccurences.js
+++ evenOccurences/evenOccurences.js
@@ -6,7 +6,7 @@
 
 /*
  *  * example usage:
- *   * var onlyEven = evenOccurence([1, 7, 2, 4, 5, 6, 8, 9, 6, 4]);
+ *   * const onlyEven = evenOccurence([1, 7, 2, 4, 5, 6, 8, 9, 6, 4]);
  *    * console.log(onlyEven); //  4
  *    */
 
