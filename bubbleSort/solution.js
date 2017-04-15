023d6b51b553a1968be54edf5c82ad111f297396
sunjieming <ben@lambdaschool.com>
2017-04-15 11:45:57 -0600

    Update variable name

--- bubbleSort/solution.js
+++ bubbleSort/solution.js
@@ -3,16 +3,16 @@ const nums = [10, 30, 5, 1, 99, 4, 9104, 32, 0, -4, 23, 0.45];
 const bubbleSort = (arr) => {
   let swappedValue;
   do {
-    swapped = false;
+    swappedValue = false;
     for (let i = 0; i < arr.length - 1; i++) {
       if (arr[i] > arr[i + 1]) {
         let temp = arr[i];
         arr[i] = arr[i + 1];
         arr[i + 1] = temp;
-        swapped = true;
+        swappedValue = true;
       }
     }
-  } while (swapped);
+  } while (swappedValue);
   return arr;
 }
 
