4ff5adaa53dac71abf69d7d60bec5ecaf57806f0
sunjieming <ben@lambdaschool.com>
2017-04-18 10:47:58 -0600

    Change const to let

--- bubbleSort/solution.js
+++ bubbleSort/solution.js
@@ -6,7 +6,7 @@ const bubbleSort = (arr) => {
     swappedValue = false;
     for (let i = 0; i < arr.length - 1; i++) {
       if (arr[i] > arr[i + 1]) {
-        const temp = arr[i];
+        let temp = arr[i];
         arr[i] = arr[i + 1];
         arr[i + 1] = temp;
         swappedValue = true;
