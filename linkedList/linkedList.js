7b435d0500f79b2dfe57e56ddcd5e388e2f02cdf
sunjieming <ben@lambdaschool.com>
2017-04-28 05:56:38 -0600

    add linkedList

---
+++ linkedList/linkedList.js
@@ -0,0 +1,16 @@
+/*
+ * Implement a linked list using an ES6 class.
+ * 
+ * Your linked list should have methods called "addToTail", "removeHead", and "contains."
+ *
+ */
+
+// const list = new LinkedList();
+// list.tail;						// returns null
+// list.addToTail('a');
+// list.addToTail('b');
+// list.head;						// returns 'a';
+// list.contains('b');  // returns true;
+// list.contains('c');  // returns false;
+// list.removeHead();		// returns 'a'
+// list.tail;						// returns 'b';
