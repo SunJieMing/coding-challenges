c38f6bb80ec6b3327285e67efdc9160b7770600b
sunjieming <ben@lambdaschool.com>
2017-05-02 09:58:03 -0600

    Add largestContiguousSum

---
+++ largetsContiguousSum/largestContiguousSum.js
@@ -0,0 +1,7 @@
+/*
+ * Find the greatest contiguous sum of numbers in an array of numbers.
+ *
+ * example 1: sumArray([1, 2, 3]); // => 6
+ * example 2: sumArray([4, -1, 5]); // => 8
+ * example 3: sumArray([10, -11, 11]); // 11
+ */
