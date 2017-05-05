8ee11161ece2d7ba88bcdc60f898dd96119bb0f9
sunjieming <ben@lambdaschool.com>
2017-05-05 10:02:38 -0600

    Add testing

---
+++ testing/fizzBuzz.js
@@ -0,0 +1,15 @@
+/*
+ * Create the function fizzBuzz that takes a single integer as an argument.
+ * Return 'fizz' if num is divisible by 3 with no remainder.
+ * Return 'buzz' if num is divisible by 5 with no remainder.
+ * Return 'fizzbuzz' if num is divisible by both 3 and 5 with no remainder.
+ * Before you write your function write tests inside of test.js to test this function.
+ * The function is already being exported and imported for you.
+ * Run your unit tests with the command `npm test` from inside of this folder.
+ */
+
+const fizzBuzz = (num) => {
+
+};
+
+module.exports = fizzBuzz;
---
+++ testing/test.js
@@ -0,0 +1,1 @@
+const fizzBuzz = require('./fizzBuzz');
