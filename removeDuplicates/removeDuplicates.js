d6ce3ddd201360e6746ec311b8609f117875009a
sunjieming <ben@lambdaschool.com>
2017-05-06 10:08:14 -0600

    Add removeDuplicates

---
+++ removeDuplicates/removeDuplicates.js
@@ -0,0 +1,7 @@
+/*
+ * Write a function that removes all duplicate values from an array.
+ * ['a', 'a', 'b', 'c', 'c'] -> ['a', 'b', 'c']
+ * [1, 1, 2, 2, 3, 3] -> [1, 2, 3]
+ * Write unit tests to test the output of this function.
+ * Run `npm init` to create a package.json file and then run `npm i --save jest`.
+ * You then need to modify the `test` script inside of `package.json` to say `jest` on the value side of the colon.*/
