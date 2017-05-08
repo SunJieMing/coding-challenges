7b9f482583af1623727289850dd851750b8e7385
sunjieming <ben@lambdaschool.com>
2017-05-08 10:01:58 -0600

    add linkedListInsertion

---
+++ linkedListInsertion/linkedListInsertion.js
@@ -0,0 +1,8 @@
+/*
+ * Create a linked list that maintains a sorted order to it's elements.
+ * When you insert a new element insert it at it's appropriate position.
+ * Example: [1] -> [2] -> [3]
+ * list.insert(3);
+ * [1] -> [2] -> [2] -> [3]
+ * Bonus: Write automated tests
+ */
