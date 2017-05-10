a1876768c47b4dfd46c9d660a69d3b342f4ecff3
sunjieming <ben@lambdaschool.com>
2017-05-10 10:01:44 -0600

    Add commonCharacters

---
+++ commonCharacters/commonCharacters.js
@@ -0,0 +1,8 @@
+/**
+ * Write a function that accepts two strings and returns a
+ * string containing all of the common characters found in both strings.
+ *
+ * Example: commonCharacters('abcdefg', 'xyzabcde')
+ * Returns: 'abcde'
+ *
+ */
