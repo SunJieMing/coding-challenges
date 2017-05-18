5bf30628bb894c8c28bef2195e6c6024a93b5ac4
sunjieming <ben@lambdaschool.com>
2017-05-18 10:02:11 -0600

    Add deepEquality

---
+++ deepEquality/deepEquality.js
@@ -0,0 +1,10 @@
+/**
+  * Write a function that, given two arrays, returns whether or not the two
+  * are deeply equivalent--meaning the contents of the two arrays is the
+  * same.  Assume there will be no objects.
+  *
+  * Examples:
+  *
+  * deepEquals([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]); // true
+  * deepEquals([1, 2, 3, 4, 5],[1, 3, 4, 5, 6]); // false
+  */
