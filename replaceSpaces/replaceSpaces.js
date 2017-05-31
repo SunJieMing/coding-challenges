2f74808c30f5de2db5fb50c0903723da3ecc1c7d
taithethai <tai@taithethai.com>
2017-05-31 12:07:56 -0400

    20170531

--- replaceSpaces/replaceSpaces.js
+++ replaceSpaces/replaceSpaces.js
@@ -1,7 +1,9 @@
 /*
- * Write a function that accepts a string and replaces all spaces witht the string '%20'.
+ * Write a function that accepts a string and replaces all spaces with the string '%20'.
  * Example: 'Mrs Jane Doe' -> 'Mrs%20Jane%20Doe'
  *
  * Note: Spaces in URLs are converted into %20.
  *
  */
+
+const replaceSpaces = str => str.replace(/ /g, "%20");
