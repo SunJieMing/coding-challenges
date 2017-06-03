707103a08055e0a53d5946c139f0a91694718d26
sunjieming <ben@lambdaschool.com>
2017-06-03 09:06:21 -0700

    Add routeBetweenNodes

---
+++ routeBetweenNodes/routeBetweenNodes.js
@@ -0,0 +1,4 @@
+/*
+ * Given a directed graph, design an algorithm to find out whether there is a route between two nodes.
+ * Google "directed graph" and look under images for visual examples.
+ */
