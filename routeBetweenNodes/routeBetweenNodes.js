a0ebfc44ad16fe7f3aef42bbe31ebdcc4ff3b424
taithethai <tai@taithethai.com>
2017-06-05 21:00:15 -0400

    current

--- routeBetweenNodes/routeBetweenNodes.js
+++ routeBetweenNodes/routeBetweenNodes.js
@@ -2,3 +2,23 @@
  * Given a directed graph, design an algorithm to find out whether there is a route between two nodes.
  * Google "directed graph" and look under images for visual examples.
  */
+
+const routeBetweenNodes = (startNode, endNode) => {
+  const roadMap = {};
+  const queue = [startNode];
+
+  const toPath = (node) => {
+    if (roadMap[node.value]) {
+      return;
+    } else {
+      roadMap[node.value] = true;
+      node.to.forEach(item => queue.push(item));
+    }
+  };
+
+  while (queue.length) {
+    if (roadMap[endNode.value]) return true;
+    toPath(queue.shift());
+  }
+  return false;
+};
