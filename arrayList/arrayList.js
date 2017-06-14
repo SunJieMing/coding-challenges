74d3413cb33dfcde84136d11836985c1f6325951
sunjieming <ben@lambdaschool.com>
2017-06-14 09:01:43 -0700

    Add arrayList

---
+++ arrayList/arrayList.js
@@ -0,0 +1,8 @@
+/*
+ * Make a class called Array.
+ * It should have the methods push, pop, get(index), and delete(index).
+ * In your implementation use a JS object to build the array.
+ * Do NOT use an array as the underlying data structure.
+ * How do these operations compare to that of a linked list?
+ * When would you want to use an array vs a linked list?
+ */
